edb15ffc1dc78810500bf67ea588398cb5941168
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-28 20:00:43 +0530

    Good Commit

--- app.js
+++ app.js
@@ -1,35 +1,75 @@
 // jshint esversion: 6
 
- const express = require("express");
- const bodyparser = require("body-parser");
+const express = require("express");
+const bodyparser = require("body-parser");
 
- const app = express();
+const app = express();
 
- app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
- app.get("/", function(req, res) {
-   var today = new Date();
-   var currentDay = today.getDay();
-   var day = "";
-
-   if(currentDay === 6 || currentDay === 0) {
-     day = "weekend";
-   } else {
-     day = "weekday";
-   }
-
-   res.render("list", {Day: day});
-
- });
+app.get("/", function(req, res) {
+  var today = new Date();
 
 
+  var option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
+  var day = today.toLocaleDateString("en-US", option);
 
+  res.render("list", {
+    Day: day
+  });
 
+});
 
 
 
 
- app.listen(3000, function() {
-   console.log("Server is running on port 3000");
- });
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
+
+// :------>
+
+// var currentDay = today.getDay();
+// var day = "";
+
+// switch (currentDay) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     break;
+//   default:
+//     day = "oops! error";
+// }
+
+// if(currentDay === 6 || currentDay === 0) {
+//   day = "weekend";
+// } else {
+//   day = "weekday";
+// }
