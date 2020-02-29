499212416b83fcd113da796497ef9927dfe9138f
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-29 19:26:02 +0530

    new workList

--- app.js
+++ app.js
@@ -3,43 +3,52 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejsLint = require('ejs-lint');
-
 const app = express();
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
 app.set('view engine', 'ejs');
 
 let items = [];
+let workItem = [];
+
 
 app.get("/", function(req, res) {
   let today = new Date();
-
   let option = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-
   let day = today.toLocaleDateString("en-US", option);
 
   res.render("list", {
-    Day: day, newListItems: items
+    listTitle: day,
+    newListItems: items
   });
 });
 
+
 app.post("/", function(req, res) {
   let item = req.body.newItems;
-  items.push(item);
 
-  res.redirect("/");
+  if (req.body.list === "WorkList") {
+    workItem.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 
+app.get("/work", function(req, res) {
 
-
-
+  res.render("list", {
+    listTitle: "WorkList",
+    newListItems: workItem
+  });
+});
 
 
 
